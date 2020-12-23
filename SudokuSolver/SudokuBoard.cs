30b31c941d7b5247748356d4d479725e3c6df1a1
Thomas Eder <thomas.e9009@gmail.com>
2020-12-23 10:07:21 +0100

    added visualisation

--- SudokuSolver/SudokuBoard.cs
+++ SudokuSolver/SudokuBoard.cs
@@ -26,7 +26,25 @@ namespace SudokuSolver {
       }
     }
 
-    public void SetNumber(int i, int j, int number, bool fixedNumber = false) {
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    sb.Append(board[i, j]);
+                    sb.Append(" ");
+                }
+                sb.Append("\r\n");
+                
+                }
+
+            return sb.ToString();
+        }
+
+        public void SetNumber(int i, int j, int number, bool fixedNumber = false) {
       board[i, j] = number;
       fixedNumbers[i, j] = fixedNumber;
     }
